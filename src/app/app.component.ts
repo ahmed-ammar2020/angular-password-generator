eeddf8eb79d8e431c348fe035293fef3d5aa8b62
ahmed-ammar2020 <ahmed.ammar0134@gmail.com>
2021-10-11 17:22:18 +0200

    pw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  passwordLength: string = "";
+  useLetters: boolean = false;
+  useNumbers: boolean = false;
+  userSymbols: boolean = false;
+  generatedPass: string = "";
+
+  onGeneratePassword() {
+    const letters = "qwertyuioplkjhgfdsazxcvbnm";
+    const numbers = "0123456789";
+    const symbols = "!@~#$%^&*()_+{}:?><";
+    let allowedChars = "";
+    let myPass = "";
+
+    if (this.useLetters) {
+      allowedChars += letters;
+    }
+
+    if (this.useNumbers) {
+      allowedChars += numbers;
+    }
+
+    if (this.userSymbols) {
+      allowedChars += symbols;
+    }
+
+    for (let i = 0; i < +this.passwordLength; i++) {
+      let randomIndex = Math.floor(Math.random() * allowedChars.length);
+      myPass += allowedChars[randomIndex];
+    }
+
+    this.generatedPass = myPass;
+
+  }
 }
